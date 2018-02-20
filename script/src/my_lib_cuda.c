e141718e7050a0690ca8aba02110066015e9251c
Pushpendre Rastogi <pushpendre@jhu.edu>
2018-02-20 08:16:45 -0500

    The gpu version of indexed sum works now.

--- script/src/my_lib_cuda.c
+++ script/src/my_lib_cuda.c
@@ -1,26 +1,27 @@
 #include <THC/THC.h>
-// this symbol will be resolved automatically from PyTorch libs
 extern THCState *state;
-int indexedsum_forward_cuda(THCudaTensor *input1, THCudaTensor *input2, THCudaTensor *output)
+
+int indexedsum_forward_cuda(THCudaTensor *input1, THCudaLongTensor *increments, THCudaTensor *output)
 {
-  for(unsigned long i=0, counter=0, n=THCudaLongTensor_nElement(increments); i < n; i++){
-    long increment = THCudaTensor_fastGet1d(increments, i);
+  for(unsigned long i=0, counter=0, n=THCudaLongTensor_nElement(state, increments); i < n; i++){
+    long increment = THCudaLongTensor_get1d(state, increments, i);
     float val = 0;
-    for(long j=0; j < increment; j++)
-      val += THTensor_fastGet1d(input1, counter+j);
-    THTensor_fastSet1d(output, i, val);
+    for(long j=0; j < increment; j++){
+      val += THCudaTensor_get1d(state, input1, counter+j);
+    }
+    THCudaTensor_set1d(state, output, i, val);
     counter += increment;
   }
   return 1;
 }
 
-int indexedsum_backward_cuda(THCudaTensor *grad_output, THCudaTensor *grad_input)
+int indexedsum_backward_cuda(THCudaTensor *grad_output, THCudaLongTensor* increments, THCudaTensor *grad_input)
 {
-  for(unsigned long i=0, counter=0, n=THCudaLongTensor_nElement(increments); i < n; i++){
-    long increment = THTensor_fastGet1d(increments, i);
-    float val = THTensor_fastGet1d(grad_output, i);
+  for(unsigned long i=0, counter=0, n=THCudaLongTensor_nElement(state, increments); i < n; i++){
+    long increment = THCudaLongTensor_get1d(state, increments, i);
+    float val = THCudaTensor_get1d(state, grad_output, i);
     for(long j=0; j < increment; j++)
-      THTensor_fastSet1d(grad_input, counter+j, val);
+      THCudaTensor_set1d(state, grad_input, counter+j, val);
     counter += increment;
   }
   return 1;
--- script/test.py
+++ script/test.py
@@ -9,8 +9,8 @@ l.backward()
 assert list(a.grad.cpu().data.numpy()) == [1.0, 1.0, 1.0, 2.0]
 
 if torch.cuda.is_available():
-    a = Variable(torch.CudaTensor([0.1, 0.2, 0.3, 0.4]), requires_grad=True)
-    b = Variable(torch.CudaLongTensor([3, 1]), requires_grad=False)
+    a = Variable(torch.cuda.FloatTensor([0.1, 0.2, 0.3, 0.4]), requires_grad=True)
+    b = Variable(torch.cuda.LongTensor([3, 1]), requires_grad=False)
     c = IndexedSumFunction()(a, b)
     l = (c[0]*1+c[1]*2)
     l.backward()
